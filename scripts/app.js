607e4a30d8908d3350417741e6f4d217523e9a61
Jitendra Sabat <jitendra.sabat@ametek.com>
2021-02-08 11:52:52 +0530

    settings icon and js functionality started
    settings icon and js functionality started

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,11 @@
 (function(){
     $(document).ready(function(){
-        
+        $(".seetingsIcon").click(function (e) {             
+            alert("Open settings right side pane");
+        });
+        $(".userNumber").click(function (e) { 
+            alert("Open tooltip");            
+        });
     });
 })();
 
