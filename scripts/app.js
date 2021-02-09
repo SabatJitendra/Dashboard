17f51792be331ae2c6c41b5d67215ae19b44025d
Jitendra Sabat <jitendra.sabat@ametek.com>
2021-02-09 23:52:24 +0530

    Integrated right side popup
    Integrated right side popup

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,38 @@
 (function(){
-    $(document).ready(function(){
-        $(".seetingsIcon").click(function (e) {             
-            alert("Open settings right side pane");
-        });
+    $(document).ready(function(){        
         $(".userNumber").click(function (e) { 
             alert("Open tooltip");            
         });
+        configureModal();
     });
+
+    function configureModal(){
+            // Get the modal
+            var modal = document.getElementById("myModal");
+            
+            // Get the button that opens the modal
+            var btn = document.getElementById("settingsButton");            
+
+            // Get the <span> element that closes the modal
+            var span = document.getElementsByClassName("close")[0];
+
+            // When the user clicks on the button, open the modal
+            btn.onclick = function() {
+                modal.style.display = "block";
+            }
+
+            // When the user clicks on <span> (x), close the modal
+            span.onclick = function() {
+                modal.style.display = "none";
+            }
+
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    }
 })();
 
 /********************************User defined function section**************************************************/
